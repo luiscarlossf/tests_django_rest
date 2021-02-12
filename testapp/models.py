f6aa1bbc6dc4494c0d9a1896c8e6c0c8270efd6d
luis <luiscarlos.sf2017@gmail.com>
2021-02-12 11:04:57 -0300

    Add Restaurant and Place models.

--- testapp/models.py
+++ testapp/models.py
@@ -4,6 +4,7 @@ from .enum_types import CountryList
 class CountryCode(models.Model):
     code = models.CharField(max_length=2,
                             choices=[(code, name) for name, code in CountryList.get_alpha_2_country().items()])
+    
 
     def __str__(self):
         return self.code
@@ -16,7 +17,7 @@ class Connector(models.Model):
     tariffs = models.ManyToManyField(Tariff, blank=True, related_name="connectors")
 
 class Location(models.Model):
-    country_code = models.ForeignKey(CountryCode,on_delete=models.CASCADE)
+    country_code = models.ForeignKey(CountryCode,on_delete=models.CASCADE, related_name="locations")
 
     @property
     def groups(self):
@@ -53,3 +54,22 @@ class User(models.Model):
     )
 
 
+class Restaurant(models.Model):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
+
+class Place(models.Model):
+    restaurant = models.OneToOneField(
+        Restaurant,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
